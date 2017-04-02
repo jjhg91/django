974962ab62ed3601c74a8d63726af4c63ee36da7
jjhg91 <jjhg91@gmail.com>
2017-04-02 19:24:30 -0400

    comenzando agregar plantillas base y estilos sass base, agregados archivos js, font, iconos a sus carpetas en static

--- finish/settings.py
+++ finish/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'finish.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-MEDIA_ROOT = 'C:/Users/JHG/Desktop/desarrolloF/finish/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')#'C:/Users/JHG/Desktop/desarrolloF/finish/media/'
 MEDIA_URL = '/media/'
 
--- finish/urls.py
+++ finish/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import url, include
+from django.contrib import admin
 from django.conf.urls.static import static
 
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^inicio/', include('apps.sports.urls')),
 ] 
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
